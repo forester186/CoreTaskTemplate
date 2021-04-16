c6198b8bbe38fc18575b3b5a88b5ab1f195b5447
forester186 <lesnaya186@gmail.com>
2021-04-16 19:28:39 +0500

    Commit

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,25 +1,29 @@
 package jm.task.core.jdbc;
 
-import jm.task.core.jdbc.service.UserServiceImpl;
+//import jm.task.core.jdbc.service.UserServiceImpl;
+
+import jm.task.core.jdbc.util.Util;
 
 public class Main {
     public static void main(String[] args) {
-        UserServiceImpl userService = new UserServiceImpl();
-//        // Создание таблицы User(ов)
-        userService.createUsersTable();
-//        //  Добавление 4 User(ов) в таблицу
-        userService.saveUser("Alexander","Pushkin",(byte)37);
-        userService.saveUser("Fyodor", "Dostoevsky", (byte)59);
-        userService.saveUser("Mikhail", "Lermontov", (byte)26);
-        userService.saveUser("Sergey", "Yesenin", (byte)30);
-        //Получение всех User из базы и вывод в консоль
-        System.out.println(userService.getAllUsers());
-        //Очистка таблицы User(ов)
-        userService.cleanUsersTable();
-        //Удаление таблицы
-        userService.dropUsersTable();
+//        UserServiceImpl userService = new UserServiceImpl();
+////        // Создание таблицы User(ов)
+//        userService.createUsersTable();
+////        //  Добавление 4 User(ов) в таблицу
+//        userService.saveUser("Alexander","Pushkin",(byte)37);
+//        userService.saveUser("Fyodor", "Dostoevsky", (byte)59);
+//        userService.saveUser("Mikhail", "Lermontov", (byte)26);
+//        userService.saveUser("Sergey", "Yesenin", (byte)30);
+//        //Получение всех User из базы и вывод в консоль
+//        System.out.println(userService.getAllUsers());
+//        //Очистка таблицы User(ов)
+//        userService.cleanUsersTable();
+//        //Удаление таблицы
+//        userService.dropUsersTable();
+//
+////        userDaoJDBC.removeUserById(2);
 
-//        userDaoJDBC.removeUserById(2);
+        Util.getSessionFactory();
 
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,34 +1,51 @@
 package jm.task.core.jdbc.util;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+
+import java.util.Properties;
 
 public class Util {
     // реализуйте настройку соеденения с БД
-    private static final String URL = "jdbc:mysql://localhost/project";
+    private static final String URL = "jdbc:mysql://localhost:3306/project";
     private static final String USERNAME = "root";
     private static final String PASSWORD = "6K$-4#Pw";
     private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String DIALECT = "org.hibernate.dialect.MySQL5Dialect";
 
-    public static void getBasaDate() {
-        try{
-            Class.forName(DRIVER).getDeclaredConstructor().newInstance();
-            getConnection();
+    public static SessionFactory sessionFactory;
+
+    public static SessionFactory getSessionFactory() {
+        try {
+
+            Configuration configuration = new Configuration();
+
+            Properties properties = new Properties();
+            properties.put(Environment.DRIVER, DRIVER);
+            properties.put(Environment.URL, URL);
+            properties.put(Environment.USER, USERNAME);
+            properties.put(Environment.PASS, PASSWORD);
+            properties.put(Environment.FORMAT_SQL, "true");
+            properties.put(Environment.DIALECT, DIALECT);
+            properties.put(Environment.SHOW_SQL, "true");
+            properties.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+
+            configuration.setProperties(properties);
+            configuration.addAnnotatedClass(User.class);
+
+            StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties());
+            sessionFactory = configuration.buildSessionFactory(builder.build());
+
+
+
+        } catch (Exception e) {
+            System.out.println("sessionFactory не создан");
+            e.printStackTrace();
         }
-        catch(Exception e){
-            System.out.println(e);
-        }
-
+        System.out.println("Ура");
+        return sessionFactory;
     }
-
-    public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL, USERNAME, PASSWORD);
-    }
-    public static Statement getStatement() throws SQLException {
-        return getConnection().createStatement();
-
-    }
-
 }
