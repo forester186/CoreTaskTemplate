b7c5d2124f45853f314e8dad0133db93044df769
forester186 <lesnaya186@gmail.com>
2021-04-17 16:27:50 +0500

    Commit

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -3,24 +3,25 @@ package jm.task.core.jdbc;
 //import jm.task.core.jdbc.service.UserServiceImpl;
 
 import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
+import jm.task.core.jdbc.service.UserServiceImpl;
 import jm.task.core.jdbc.util.Util;
 
 public class Main {
     public static void main(String[] args) {
-        UserDaoHibernateImpl userDaoHibernate = new UserDaoHibernateImpl();
+        UserServiceImpl userService = new UserServiceImpl();
         // Создание таблицы User(ов)
-        userDaoHibernate.createUsersTable();
+        userService.createUsersTable();
         //  Добавление 4 User(ов) в таблицу
-        userDaoHibernate.saveUser("Alexander","Pushkin",(byte)37);
-        userDaoHibernate.saveUser("Fyodor", "Dostoevsky", (byte)59);
-        userDaoHibernate.saveUser("Mikhail", "Lermontov", (byte)26);
-        userDaoHibernate.saveUser("Sergey", "Yesenin", (byte)30);
+        userService.saveUser("Alexander","Pushkin",(byte)37);
+        userService.saveUser("Fyodor", "Dostoevsky", (byte)59);
+        userService.saveUser("Mikhail", "Lermontov", (byte)26);
+        userService.saveUser("Sergey", "Yesenin", (byte)30);
         //Получение всех User из базы и вывод в консоль
-        System.out.println(userDaoHibernate.getAllUsers());
+        System.out.println(userService.getAllUsers());
 //        //Очистка таблицы User(ов)
-        userDaoHibernate.cleanUsersTable();
+        userService.cleanUsersTable();
 //        //Удаление таблицы
-         userDaoHibernate.dropUsersTable();
+        userService.dropUsersTable();
 
 //        userDaoHibernate.removeUserById(2);
 
